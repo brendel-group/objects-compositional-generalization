b357952d3d63199535e0f1d140881e4a5758a386
Alexander Panfilov <sasha_pusha@mail.ru>
2022-12-02 11:18:38 +0100

    r2_score added

--- src/train.py
+++ src/train.py
@@ -5,6 +5,8 @@ import torchvision.transforms as transforms
 from scipy.optimize import linear_sum_assignment
 from torch.utils.tensorboard import SummaryWriter
 
+from torchmetrics import R2Score
+
 import torchvision
 
 writer = SummaryWriter()
@@ -14,10 +16,26 @@ from . import data
 from . import models
 
 
+def calculate_r2_score(true_latents, predicted_latents, inds):
+    """Calculate R2 score. Slots are flattened before calculating R2 score."""
+    inds = torch.LongTensor(inds)
+    predicted_latents = predicted_latents.detach().cpu()
+    true_latents = true_latents.detach().cpu()
+
+    for i in range(true_latents.shape[0]):
+        predicted_latents[i, :] = predicted_latents[i, inds[i, :, 1], ...]
+
+    true_latents = true_latents.view(true_latents.shape[0], -1)
+    predicted_latents = predicted_latents.reshape(predicted_latents.shape[0], -1)
+
+    r2 = R2Score(true_latents.shape[1])
+    return r2(true_latents, predicted_latents)
+
+
 def matched_slots_loss(true_latents, predicted_latents, device="cpu", p=2):
     """
     Computes pairwise distance between slots, matches slots with Hungarian algorithm and outputs
-    sum of distances.
+    sum of distances ^ p.
     """
     pairwise_cost = torch.pow(
         torch.cdist(true_latents, predicted_latents, p=p).transpose(-1, -2), p
@@ -28,17 +46,16 @@ def matched_slots_loss(true_latents, predicted_latents, device="cpu", p=2):
     transposed_indices = torch.from_numpy(np.transpose(indices, axes=(0, 2, 1)))
 
     # extracting the cost of the matched slots; this code is a bit ugly, idk what is the nice way to do it
-    output = (
-        torch.gather(pairwise_cost, 2, transposed_indices.to(device))[:, :, -1]
-        .sum()
-    )
+    output = torch.gather(pairwise_cost, 2, transposed_indices.to(device))[
+        :, :, -1
+    ].sum()
     return output, transposed_indices
 
 
 def matched_latents_loss(true_latents, predicted_latents, device="cpu", p=2):
     """
     Computes pairwise distance between flattened latents, matches latents with Hungarian algorithm and outputs
-    sum of distances.
+    sum of distances ^ p.
     """
     pairwise_cost = torch.pow(
         torch.cdist(
@@ -88,6 +105,7 @@ def test(model, test_loader, device, epoch):
     """Test the model on test set. Currently only supports MSE loss."""
     model.eval()
     test_loss = 0
+    r2_score = 0
     with torch.no_grad():
         for batch_idx, (data, true_latents) in enumerate(test_loader):
             data = data.to(device)
@@ -99,11 +117,15 @@ def test(model, test_loader, device, epoch):
                 loss, inds = matched_latents_loss(
                     predicted_latents, true_latents, device
                 )
-
+            r2_score += calculate_r2_score(true_latents, predicted_latents, inds)
             test_loss += loss.item()
-    print("====> Test set loss: {:.4f}".format(test_loss / len(test_loader.dataset)))
+    print(
+        "====> Test set loss: {:.4f}, r2 score {:.4f}".format(
+            test_loss / len(test_loader.dataset), r2_score / len(test_loader.dataset)
+        ),
+    )
     writer.add_scalar("Average Loss/test", test_loss / len(test_loader.dataset), epoch)
-
+    writer.add_scalar("R2 Score/test", r2_score / len(test_loader.dataset), epoch)
 
 def collate_fn_normalizer(batch, bias=0, scale=1):
     """Normalize latents target to [0, 1]."""
@@ -178,7 +200,7 @@ def run():
     writer.add_image("train_images", img_grid)
     writer.add_graph(model, images.to(device))
 
-    for epoch in range(1, 1000 + 1):
+    for epoch in range(1, 2000 + 1):
         train(model, train_loader, optimizer, device, epoch)
-        if epoch % 10 == 0:
+        if epoch % 50 == 0:
             test(model, test_loader, device, epoch)
