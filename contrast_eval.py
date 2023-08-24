contrast_eval.py fixed; wandb calls commented; slot_attention.py adapted so it is easy to remove sampling and softmax;

contrast_eval.py
@@ -1,5 +1,6 @@
 from src.models import base_models, slot_attention
 from src.datasets import utils as data_utils
+import src.metrics as metrics
 
 import os
 import src
@@ -7,13 +8,10 @@ import torch
 
 # from functorch import jacfwd
 from torch.func import jacfwd
-from tqdm import tqdm
 
 import numpy as np
+import pickle
 
-
-import functorch
-print(functorch.__version__)
 
 def load_model_and_hook(path, model_name):
     # Load the checkpoint
@@ -40,6 +38,8 @@ def load_model_and_hook(path, model_name):
             num_iterations=3,
             hid_dim=16,
             dataset_name="dsprites",
+            sampling=True,
+            softmax=True,
         )
         decoder_hook = model.decode
     elif model_name == "SlotMLPAdditive":
@@ -58,10 +58,16 @@ def load_model_and_hook(path, model_name):
 def compositional_contrast(jac, slot_dim):
     batch_size, obs_dim, z_dim = jac.shape[0], jac.shape[1], jac.shape[2]
     num_slots = int(z_dim / slot_dim)
-    jac = jac.reshape(batch_size * obs_dim, z_dim)  # batch_size*obs_dim x num_slots*slot_dim
-    slot_rows = torch.stack(torch.split(jac, slot_dim, dim=1))  # num_slots x batch_size*obs_dim x slot_dim
+    jac = jac.reshape(
+        batch_size * obs_dim, z_dim
+    )  # batch_size*obs_dim x num_slots*slot_dim
+    slot_rows = torch.stack(
+        torch.split(jac, slot_dim, dim=1)
+    )  # num_slots x batch_size*obs_dim x slot_dim
     slot_norms = torch.norm(slot_rows, dim=2)  # num_slots x batch_size*obs_dim
-    slot_norms = slot_norms.view(num_slots, batch_size, obs_dim).permute(1, 0, 2)  # batch_size x num_slots x obs_dim
+    slot_norms = slot_norms.view(num_slots, batch_size, obs_dim).permute(
+        1, 0, 2
+    )  # batch_size x num_slots x obs_dim
     slot_norms += 1e-12
     slot_norms_max = slot_norms.sum(1) / num_slots
     slot_norms_norm = slot_norms / slot_norms_max.unsqueeze(1).repeat(1, num_slots, 1)
@@ -76,7 +82,7 @@ def compositional_contrast(jac, slot_dim):
             comp_conts_norm += slot_norms_norm[:, i] * slot_norms_norm[:, j + 1]
     comp_cont = (comp_conts).sum(1).mean()
     comp_cont_norm = (comp_conts_norm).sum(1).mean()
-    weight_comp_cont_norm = ((comp_conts_norm) * weights).sum(1).mean()
+    weight_comp_cont_norm = ((comp_conts_norm) * weights).sum(1)
 
     return comp_cont, comp_cont_norm, weight_comp_cont_norm
 
@@ -85,35 +91,62 @@ def cast_models_to_cuda(models):
     for model in models:
         model.cuda()
 
-def comp_eval(id_loader, ood_loader, models, hooks):
-    def process_loader(loader, model, decoder_hook):
-        accum_comp_contrast = 0
-        for (images, latents) in tqdm(loader):
-            images = images[:, -1, ...].to("cuda")
-            out = model(images)
-            latents = out["predicted_latents"]
 
-            jac = jacfwd(decoder_hook)(latents)
-            jac_right = jac[0].flatten(1, 4).flatten(2, 3) # taking the reconstruction jacobian
+def calculate_contrast(out, decoder_hook):
+    """
+    Calculate the contrast score
+    """
+    latents = out["predicted_latents"]
 
-            (_, _, weighted_comp) = compositional_contrast(jac_right, 16)
+    jac = jacfwd(decoder_hook)(latents)
+    jac_right = jac[0].flatten(1, 4).flatten(2, 3)  # taking the reconstruction jacobian
 
-            accum_comp_contrast += weighted_comp.detach().cpu().numpy()
-        return accum_comp_contrast
+    (_, _, weighted_comp) = compositional_contrast(jac_right, 16)
 
-    contrasts = []
-    for model, decoder_hook in zip(models, hooks):
-        accum_comp_contrast_id = process_loader(id_loader, model, decoder_hook)
-        accum_comp_contrast_ood = process_loader(ood_loader, model, decoder_hook)
-        contrasts.append((accum_comp_contrast_id, accum_comp_contrast_ood))
+    return weighted_comp.detach().cpu().numpy()
 
-    return contrasts
+
+def calculate_identifiability(id_loader, ood_loader, model):
+    """
+    Calculate the identifiability score
+    """
+    id_score_id, id_score_ood = metrics.identifiability_score(
+        model,
+        id_loader,
+        ood_loader,
+        [2],
+        "cuda",
+    )
+    return id_score_id, id_score_ood
+
+
+def calculate_image_r2(images, out):
+    """
+    Calculate the image r2 score
+    """
+    x_hat = out["reconstructed_image"]
+
+    r2 = metrics.image_r2_score(true_images=images, predicted_images=x_hat)
+    return r2.detach().cpu().numpy()
+
+
+def calculate_encoder_consistency(out):
+    """
+    Calculate the encoder consistency score
+    """
+    consistency_encoder_loss, _ = metrics.hungarian_slots_loss(
+        out["sampled_latents"],
+        out["predicted_sampled_latents"],
+        "cuda",
+    )
+    return consistency_encoder_loss.detach().cpu().numpy()
+
 
 def evaluate():
     data_path = os.path.join(data_utils.data_path, "dsprites")
     n_samples_test = 5000
-    n_slots = 4
-    mixed = True
+    n_slots = 2
+    mixed = False
     delta = 0.125
     no_overlap = True
     batch_size = 5
@@ -128,73 +161,102 @@ def evaluate():
     )
 
     id_loader = wrapper.get_test_loader(
-                n_samples_test,
-                n_slots,
-                sample_mode_test_id,
-                delta,
-                no_overlap,
-                batch_size,
-                mixed=mixed,
+        n_samples_test,
+        n_slots,
+        sample_mode_test_id,
+        delta,
+        no_overlap,
+        batch_size,
+        mixed=mixed,
     )
 
     ood_loader = wrapper.get_test_loader(
-                n_samples_test,
-                n_slots,
-                sample_mode_test_ood,
-                delta,
-                no_overlap,
-                batch_size,
-                mixed=mixed,
+        n_samples_test,
+        n_slots,
+        sample_mode_test_ood,
+        delta,
+        no_overlap,
+        batch_size,
+        mixed=mixed,
     )
 
-    # paths = "/mnt/qb/work/bethge/apanfilov27/slurm_scripts/models/SlotAttention_2obj_nooverlaps_nomasks_nosampling_vanilla_600"
-    # paths_and_names = []
-    # for name in os.listdir(paths):
-    #     if name.endswith(".pt"):
-    #         paths_and_names.append((os.path.join(paths, name), "SlotAttention"))
-
-    paths = "/mnt/qb/work/bethge/apanfilov27/slurm_scripts/models/SlotAttention_MIXEDobj_vanilla_400"
+    # example of how you could load multiple models, feel free to change this
+    paths = "/mnt/qb/work/bethge/apanfilov27/slurm_scripts/models/SlotMLPAdditive_2obj_300_enccons_no_overlaps"
     paths_and_names = []
     for name in os.listdir(paths):
         if name.endswith(".pt"):
-            paths_and_names.append((os.path.join(paths, name), "SlotAttention"))
+            paths_and_names.append((os.path.join(paths, name), "SlotMLPAdditive"))
 
-    # paths = "/mnt/qb/work/bethge/apanfilov27/slurm_scripts/models/SlotAttention_2obj_400_vanilla_no_overlaps"
-    # paths_and_names = [
-    #     (os.path.join(paths, "SlotAttention_2obj_400_vanilla_no_overlaps_2023.pt"), "SlotAttention"),
-    #     (os.path.join(paths, "SlotAttention_2obj_400_vanilla_no_overlaps_2024.pt"), "SlotAttention"),
-    #     (os.path.join(paths, "SlotAttention_2obj_400_vanilla_no_overlaps_2025.pt"), "SlotAttention"),
-    #     (os.path.join(paths, "SlotAttention_2obj_400_vanilla_no_overlaps_2026.pt"), "SlotAttention"),
-    #     (os.path.join(paths, "SlotAttention_2obj_400_vanilla_no_overlaps_2027.pt"), "SlotAttention"),
-    # ]
-
-    # paths = "/mnt/qb/work/bethge/apanfilov27/slurm_scripts/models/SlotMLPAdditive_2obj_300_vanilla_no_overlaps"
-    # paths_and_names = [
-    #     (os.path.join(paths, "SlotMLPAdditive_2obj_300_vanilla_no_overlaps_2023.pt"), "SlotMLPAdditive"),
-    #     (os.path.join(paths, "SlotMLPAdditive_2obj_300_vanilla_no_overlaps_2024.pt"), "SlotMLPAdditive"),
-    #     (os.path.join(paths, "SlotMLPAdditive_2obj_300_vanilla_no_overlaps_2027.pt"), "SlotMLPAdditive"),
-    #     (os.path.join(paths, "SlotMLPAdditive_2obj_300_vanilla_no_overlaps_2028.pt"), "SlotMLPAdditive"),
-    #     (os.path.join(paths, "SlotMLPAdditive_2obj_300_vanilla_no_overlaps_2031.pt"), "SlotMLPAdditive"),
-    # ]
     models = []
     hooks = []
-    for (path, name) in paths_and_names:
+    for path, name in paths_and_names:
         model, decoder_hook = load_model_and_hook(path, name)
         models.append(model)
         hooks.append(decoder_hook)
 
     cast_models_to_cuda(models)
 
-    contrasts = comp_eval(id_loader, ood_loader, models, hooks)
+    id_id_scores = []
+    ood_id_scores = []
+    id_contrasts = []
+    ood_contrasts = []
+    id_image_r2 = []
+    ood_image_r2 = []
+    id_encoder_consistency = []
+    ood_encoder_consistency = []
 
-    id_contrasts = np.array([c[0] for c in contrasts])
-    ood_contrasts = np.array([c[1] for c in contrasts])
 
-    id_contrasts = id_contrasts * batch_size / n_samples_test
-    ood_contrasts = ood_contrasts * batch_size / n_samples_test
+    # evaluating provided models
+    for model, hook in zip(models, hooks):
+        # mean id scores
+        id_id_score, ood_id_score = calculate_identifiability(
+            id_loader, ood_loader, model
+        )
+        id_id_score, ood_id_score = 0, 0
 
-    print("ID contrasts: ", np.mean(id_contrasts), "+-", np.std(id_contrasts))
-    print("OOD contrasts: ", np.mean(ood_contrasts), "+-", np.std(ood_contrasts))
+        id_r2, ood_r2 = 0, 0
+        id_consistency, ood_consistency = 0, 0
+        id_contrast, ood_contrast = 0, 0
+
+        for i, (id_batch, ood_batch) in enumerate(zip(id_loader, ood_loader)):
+            id_images, _ = id_batch
+            id_images = id_images[:, -1, ...].cuda() # taking the last image
+
+
+            ood_images, _ = ood_batch
+            ood_images = ood_images[:, -1, ...].cuda() # taking the last imag
+
+            id_out = model(id_images)
+            ood_out = model(ood_images)
+
+            id_contrast += calculate_contrast(id_out, hook)
+            ood_contrast += calculate_contrast(ood_out, hook)
+
+            id_r2 += calculate_image_r2(id_images, id_out)
+            ood_r2 += calculate_image_r2(ood_images, ood_out)
+
+            id_consistency += calculate_encoder_consistency(id_out).mean()
+            ood_consistency += calculate_encoder_consistency(ood_out).mean()
+
+        id_contrasts.append(id_contrast * batch_size / n_samples_test)
+        ood_contrasts.append(ood_contrast * batch_size / n_samples_test)
+
+        id_image_r2.append(id_r2 * batch_size / n_samples_test)
+        ood_image_r2.append(ood_r2 * batch_size / n_samples_test)
+
+        id_encoder_consistency.append(id_consistency * batch_size / n_samples_test)
+        ood_encoder_consistency.append(ood_consistency * batch_size / n_samples_test)
+
+        id_id_scores.append(id_id_score)
+        ood_id_scores.append(ood_id_score)
+
+
+    print("id_id_scores", id_id_scores)
+    print("ood_id_scores", ood_id_scores)
+    print("id_contrasts", id_contrasts)
+    print("ood_contrasts", ood_contrasts)
+    print("id_image_r2", id_image_r2)
+    print("ood_image_r2", ood_image_r2)
 
 if __name__ == "__main__":
     evaluate()

notebooks/utils/heatmap_utils.py
@@ -1,16 +1,16 @@
-import os
 import random
 
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 import torch
 import torchvision.transforms as transforms
 import tqdm
 from torch import nn
 
-from src.datasets import data
 import src.datasets.configs as data_configs
+from src.datasets import data
+from src.models import base_models
+from src.models import slot_attention
 
 default_cfg = data_configs.SpriteWorldConfig()
 
@@ -66,16 +66,17 @@ def plot_dataset_images(dataset, rows=5, cols=5, id_mask=True):
 def create_traversed_dataset(initial_sample, n_steps=4):
     # Define the ranges for y and c_0
     y_range = torch.linspace(0.2, 0.8, n_steps)
+
     c_0_range = torch.linspace(0.05, 0.95, n_steps)
 
-    y_combinations = torch.cartesian_prod(y_range, y_range)
+    # y_combinations = torch.cartesian_prod(y_range, y_range)
     c_0_combinations = torch.cartesian_prod(c_0_range, c_0_range)
 
     # Create a tensor of repeated initial samples with the shape (n_steps, initial_sample[0], initial_sample[1])
     samples = initial_sample.repeat((n_steps**2, 1, 1))
 
-    samples[:, 0, 1] = y_combinations[:, 0]
-    samples[:, 1, 1] = y_combinations[:, 1]
+    # samples[:, 0, 1] = y_combinations[:, 0]
+    # samples[:, 1, 1] = y_combinations[:, 1]
     samples[:, 0, -3] = c_0_combinations[:, 0]
     samples[:, 1, -3] = c_0_combinations[:, 1]
 
@@ -133,10 +134,10 @@ def get_binary_id_mask(
     for x, z in dataset:
         val = (
             check_distance_from_diag(np.array([z[0][1].item(), z[1][1].item()]))
-            <= 0.125
+            <= 0.125 * 2
         ) and (
-            check_distance_from_diag(np.array([z[0][-3].item(), z[1][-3].item()]))
-            <= 0.125
+            check_distance_from_diag(np.array([z[0][-1].item(), z[1][-1].item()]))
+            <= 0.125 * 2
         )
         points.append(int(val))
 
@@ -230,3 +231,97 @@ def get_id_bounds(x, y, shape):
         np.array(x_line_right) - 0.5,
         np.array(y_line_right) - 0.5,
     )
+
+
+def load_model_and_hook(path, model_name):
+    # Load the checkpoint
+    checkpoint = torch.load(path)
+
+    # Determine which model to load based on the model name
+    if model_name == "SlotAttention":
+        encoder = slot_attention.SlotAttentionEncoder(
+            resolution=(64, 64),
+            hid_dim=16,
+            ch_dim=32,
+            dataset_name="dsprites",
+        )
+        decoder = slot_attention.SlotAttentionDecoder(
+            hid_dim=16,
+            ch_dim=32,
+            resolution=(64, 64),
+            dataset_name="dsprites",
+        )
+        model = slot_attention.SlotAttentionAutoEncoder(
+            encoder=encoder,
+            decoder=decoder,
+            num_slots=2,
+            num_iterations=3,
+            hid_dim=16,
+            dataset_name="dsprites",
+        )
+        decoder_hook = model.decode
+    elif model_name == "SlotMLPAdditive":
+        model = base_models.SlotMLPAdditive(3, 2, 16)
+        decoder_hook = model.decoder
+    else:
+        raise ValueError("Invalid model name")
+
+    # Load the model weights and set the model to eval model
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.eval()
+
+    return model, decoder_hook
+
+
+def evaluate_function_over_seeds(func, seeds, *args, **kwargs):
+    results = {"mse_model": [], "mse_decoder": [], "figures_mse_model": []}
+
+    for seed in seeds:
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+
+        temp_results = func(*args, **kwargs)
+
+        # If results lists are empty, initialize them with lists for each sample
+        if not results["mse_model"]:
+            results["mse_model"] = [
+                [] for _ in range(len(temp_results["model_performances"]))
+            ]
+            results["mse_decoder"] = [
+                [] for _ in range(len(temp_results["model_performances"]))
+            ]
+            results["figures_mse_model"] = [
+                [] for _ in range(len(temp_results["model_performances"]))
+            ]
+
+        for i in range(len(temp_results["model_performances"])):
+            results["mse_model"][i].append(
+                temp_results["model_performances"][i]["mse_model"]
+            )
+            results["mse_decoder"][i].append(
+                temp_results["model_performances"][i]["mse_decoder"]
+            )
+            results["figures_mse_model"][i].append(
+                temp_results["model_performances"][i]["figures_mse_model"]
+            )
+
+    # Compute median for each sample
+    for key in results:
+        results[key] = [
+            np.median(torch.stack(results[key][i]), axis=0)
+            for i in range(len(results[key]))
+        ]
+
+    # re-arrange the results
+    out = []
+    for i in range(len(results["mse_model"])):
+        out.append(dict())
+        for key in results:
+            out[i][key] = results[key][i]
+
+    return out
+
+def cast_models_to_device(models):
+    for model in models:
+        model.cuda()

src/models/slot_attention.py
@@ -25,6 +25,8 @@ class SlotAttentionAutoEncoder(nn.Module):
         hid_dim,
         dataset_name,
         no_overlap=False,
+        softmax=True,
+        sampling=True,
     ):
         """Builds the Slot Attention-based auto-encoder.
         Args:
@@ -51,12 +53,15 @@ class SlotAttentionAutoEncoder(nn.Module):
             iters=self.num_iterations,
             eps=1e-8,
             hidden_dim=64,
+            use_sampling=sampling,
         )
 
         if dataset_name == "dsprites":
             self.spatial_broadcast = 64
         elif dataset_name == "kubric":
             self.spatial_broadcast = 8
+
+        self.use_softmax = softmax
 
     def encode(self, x):
         # `x` is an image which has shape: [batch_size, num_channels, width, height].
@@ -91,7 +96,10 @@ class SlotAttentionAutoEncoder(nn.Module):
         ).split([3, 1], dim=-1)
 
         # Normalize alpha masks over slots.
-        masks = F.softmax(masks, dim=1)
+        if self.use_softmax:
+            masks = F.softmax(masks, dim=1)
+        else:
+            masks = torch.sigmoid(masks)
         xhs = reconstructions * masks
 
         # `hat_x` has shape: [batch_size, num_channels, width, height].
@@ -199,7 +207,9 @@ class SlotAttentionAutoEncoder(nn.Module):
 
 
 class SlotAttention(nn.Module):
-    def __init__(self, num_slots, dim, iters=3, eps=1e-8, hidden_dim=128):
+    def __init__(
+        self, num_slots, dim, iters=3, eps=1e-8, hidden_dim=128, use_sampling=True
+    ):
         super().__init__()
         self.num_slots = num_slots
         self.iters = iters
@@ -223,12 +233,24 @@ class SlotAttention(nn.Module):
         self.norm_slots = nn.LayerNorm(dim)
         self.norm_pre_ff = nn.LayerNorm(dim)
 
+        self.use_sampling = use_sampling
+
     def forward(self, inputs, num_slots=None):
         b, n, d = inputs.shape
         n_s = num_slots if num_slots is not None else self.num_slots
         mu = self.slots_mu.expand(b, n_s, -1)
         sigma = self.slots_log_sigma.expand(b, n_s, -1).exp()
-        slots = mu + sigma * torch.randn_like(sigma)
+
+        # remove randomness from slots initialization
+        if not self.use_sampling:
+            g = torch.Generator()
+            g.manual_seed(0)
+
+            slots = mu + sigma * torch.normal(0, 1, size=sigma.shape, generator=g).to(
+                sigma.device
+            )
+        else:
+            slots = mu + sigma * torch.randn_like(sigma)
 
         inputs = self.norm_input(inputs)
         k, v = self.to_k(inputs), self.to_v(inputs)

src/train.py
@@ -4,7 +4,7 @@ import time
 import torch
 import torch.utils.data
 
-import wandb
+# import wandb
 
 import src.datasets.wrappers
 import src.metrics as metrics
@@ -17,8 +17,8 @@ from contextlib import nullcontext
 
 from .models import base_models, slot_attention
 
-# TODO: remove this
-wandb.login(key="b17ca470c2ce70dd9d6c3ce01c6fc7656633fe91")
+# # TODO: remove this
+# wandb.login(key="b17ca470c2ce70dd9d6c3ce01c6fc7656633fe91")
 
 
 def one_epoch(
@@ -36,7 +36,7 @@ def one_epoch(
     use_consistency_loss=True,
     extended_consistency_loss=False,
     unsupervised_mode=False,
-    freq=10,
+    freq=1,
     **kwargs,
 ):
     """One epoch of training or testing. Please check main.py for keyword parameters descriptions'."""
@@ -198,14 +198,14 @@ def one_epoch(
         accum_consistency_encoder_loss,
         accum_consistency_decoder_loss,
     )
-    if epoch % freq == 0:
-        print(f"ARI score: {accum_ari_score:.4f}")
-        wandb_utils.wandb_log(
-            data_path,
-            kwargs["dataset_name"],
-            **output_dict,
-            **locals(),
-        )
+    # if epoch % freq == 0:
+    #     print(f"ARI score: {accum_ari_score:.4f}")
+    #     wandb_utils.wandb_log(
+    #         data_path,
+    #         kwargs["dataset_name"],
+    #         **output_dict,
+    #         **locals(),
+    #     )
 
     return accum_reconstruction_loss
 
@@ -250,20 +250,20 @@ def run(
     data_path = os.path.join(data_utils.data_path, dataset_name)
 
     signature_args = locals().copy()
-    wandb_config = signature_args
-    wandb.init(
-        config=wandb_config,
-        project="object_centric_ood",
-        dir=os.path.join(data_utils.data_path, "wandb"),
-    )
+    # wandb_config = signature_args
+    # wandb.init(
+    #     config=wandb_config,
+    #     project="object_centric_ood",
+    #     dir=os.path.join(data_utils.data_path, "wandb"),
+    # )
 
-    for mode in ["ID", "OOD", "RDM"]:
-        wandb.define_metric(f"test_{mode} reconstruction loss", summary="min")
-    wandb.define_metric("ID_score_ID", summary="max")
-    wandb.define_metric("ID_score_OOD", summary="max")
-    for mode in ["ID", "OOD", "RDM"]:
-        wandb.define_metric(f"test_{mode} ari score", summary="max")
-    wandb_utils.wandb_log_code(wandb.run)
+    # for mode in ["ID", "OOD", "RDM"]:
+    #     wandb.define_metric(f"test_{mode} reconstruction loss", summary="min")
+    # wandb.define_metric("ID_score_ID", summary="max")
+    # wandb.define_metric("ID_score_OOD", summary="max")
+    # for mode in ["ID", "OOD", "RDM"]:
+    #     wandb.define_metric(f"test_{mode} ari score", summary="max")
+    # wandb_utils.wandb_log_code(wandb.run)
 
     time_created = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
     training_utils.set_seed(seed)
@@ -329,13 +329,15 @@ def run(
             hid_dim=n_slot_latents,
             dataset_name=dataset_name,
             no_overlap=no_overlap,
+            sampling=True, # change to False for the fixed model
+            softmax=True, # change to False for the fixed model
         ).to(device)
     elif model_name == "MONet":
         model = src.models.get_monet_model(n_slots, n_slot_latents, device)
     elif model_name == "GENESIS":
         model = src.models.get_genesis_model(n_slots, n_slot_latents, device)
 
-    wandb.watch(model)
+    # wandb.watch(model)
 
     # warmup
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-7, weight_decay=0.001)
@@ -379,10 +381,10 @@ def run(
 
         print("Learning rate: ", optimizer.param_groups[0]["lr"])
 
-        if epoch % test_freq == 0:
+        if epoch % 1 == 0:
             if (
                 model_name in ["SlotAttention", "SlotMLPAdditive", "MONet", "GENESIS"]
-                and epoch % 100 == 0
+                and epoch % 1 == 0
             ):
                 if dataset_name == "dsprites":
                     categorical_dimensions = [2]
@@ -396,13 +398,13 @@ def run(
                     categorical_dimensions,
                     device,
                 )
-                wandb.log(
-                    {
-                        "ID_score_ID": id_score_id,
-                        "ID_score_OOD": id_score_ood,
-                    },
-                    step=epoch,
-                )
+                # wandb.log(
+                #     {
+                #         "ID_score_ID": id_score_id,
+                #         "ID_score_OOD": id_score_ood,
+                #     },
+                #     step=epoch,
+                # )
 
             id_rec_loss = one_epoch(
                 model,
@@ -422,30 +424,15 @@ def run(
                 **signature_args,
             )
 
-            if id_rec_loss < min_reconstruction_loss_ID:
-                min_reconstruction_loss_ID = id_rec_loss
-                print(
-                    f"\nNew best ID model!\nEpoch: {epoch}\nID reconstruction loss: {id_rec_loss}\n"
-                )
-                training_utils.save_checkpoint(
-                    path=data_utils.data_path,
-                    **locals(),
-                    checkpoint_name=f"best_id_model_{sample_mode_train}_{seed}",
-                )
+            checkpoint_save_path = data_utils.data_path # change this to your path
+            training_utils.save_checkpoint(
+                path=checkpoint_save_path,
+                **locals(),
+                checkpoint_name=f"{save_name}_{epoch}",
+            )
 
-            if ood_rec_loss < min_reconstruction_loss_OOD:
-                min_reconstruction_loss_OOD = ood_rec_loss
-                print(
-                    f"\nNew best OOD model!\nEpoch: {epoch}\nOOD reconstruction loss: {ood_rec_loss}\n"
-                )
-                training_utils.save_checkpoint(
-                    path=data_utils.data_path,
-                    **locals(),
-                    checkpoint_name=f"best_ood_model_{sample_mode_train}_{seed}",
-                )
-
-        training_utils.save_checkpoint(
-            path=data_utils.data_path,
-            **locals(),
-            checkpoint_name=save_name,
-        )
+    training_utils.save_checkpoint(
+        path=checkpoint_save_path,
+        **locals(),
+        checkpoint_name=f"{save_name}_{epoch}",
+    )